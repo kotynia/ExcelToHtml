137e50f5eacf9c569ef0dd030eec6779d555254d
Marcin kotynia <wicherqm@gmail.com>
2017-07-09 13:48:41 +0200

    Init

--- ExcelToHtml/Properties/AssemblyInfo.cs
+++ ExcelToHtml/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExcelToHtml")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Excel To HTML Parser")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Marcin Kotynia")]
 [assembly: AssemblyProduct("ExcelToHtml")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
